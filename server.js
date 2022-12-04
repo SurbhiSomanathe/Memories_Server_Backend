5e543f767cbd3609a7678606bafcdf9ab6b3f7a5
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-12-04 17:42:22 +0530

    after chnges mongo

---
+++ server.js
@@ -0,0 +1,33 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+dotenv.config();
+
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL = 'mongodb+srv://backmemo:backmemo@cluster0.dnexgzi.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://memo123:memo123@cluster0.hzvrdjm.mongodb.net/?retryWrites=true&w=majority';
+mongoose
+.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+})
+.then(() => console.log("DB Connected..!"))
+.catch(err => console.log(err))
+const PORT = process.env.PORT|| 9900;
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
+
+// mongoose.connect(CONNECTION_URL)
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  // .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
